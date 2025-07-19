33352451686133869b7b66e5549120df15ddf725
sea gin <cijin@seagin.me>
2025-07-19 12:02:16 +0700

    simple loader

--- src/main.zig
+++ src/main.zig
@@ -3,44 +3,32 @@ const stdout = std.io.getStdOut().writer();
 
 const clear_and_reset = "\x1B[2J\x1B[H";
 const toggle_cursor = "\x1B[?25l";
+const spinee = [_][]const u8{
+    "⠋",
+    "⠙",
+    "⠹",
+    "⠸",
+    "⠼",
+    "⠴",
+    "⠦",
+    "⠧",
+    "⠇",
+    "⠏",
+};
 const w = 60;
 const h = 20;
 const sleep_time = 100 * 1_000_000;
 
 pub fn main() !void {
-    var x: i32 = 0;
-    var dx: i32 = 1;
-    var y: i32 = 0;
-    var dy: i32 = 1;
-
     try stdout.print(toggle_cursor, .{});
 
-    while (true) {
+    var idx: u64 = 0;
+    while (idx >= 0 and idx <= spinee.len) : (idx += 1) {
         try stdout.print(clear_and_reset, .{});
+        try stdout.print("{s}", .{spinee[idx]});
 
-        var row: i32 = 0;
-        while (row < h) : (row += 1) {
-            var col: i32 = 0;
-            while (col < w) : (col += 1) {
-                if (row == y and col == x) {
-                    try stdout.print("0", .{});
-                    continue;
-                }
-
-                try stdout.print(" ", .{});
-            }
-            try stdout.print("\n", .{});
-        }
-
-        x += dx;
-        y += dy;
-
-        if (x >= w - 1 or x <= 0) {
-            dx = -dx;
-        }
-
-        if (y >= h - 1 or y <= 0) {
-            dy = -dy;
+        if (idx + 1 >= spinee.len) {
+            idx = (idx + 1) % spinee.len;
         }
 
         std.time.sleep(sleep_time);
